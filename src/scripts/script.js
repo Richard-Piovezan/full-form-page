24db664e944a1ccda5c8dc52295883979fef65dc
richard-piovezan <contato.piovezan@gmail.com>
2025-05-11 19:19:09 -0300

    create input file to birth documents

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,26 +1,89 @@
 AOS.init();
 
 $(document).ready(function() {
+    $('#name').mask('#', {
+        maxlength: false,
+        placeholder: 'Nome completo da criança',
+        translation: {
+            '#': {
+                pattern: /[a-zA-Z]/,
+                recursive: true
+            }
+        }
+    })
+
+    $('#responsible-name').mask('#', {
+        maxlength: false,
+        placeholder: 'Nome completo do responsável',
+        translation: {
+            '#': {
+                pattern: /[a-zA-Z]/,
+                recursive: true
+            }
+        }
+    })
+
+    $('#city').mask('#', {
+        maxlength: false,
+        placeholder: 'Nome completo da cidade',
+        translation: {
+            '#': {
+                pattern: /[a-zA-Z]/,
+                recursive: true
+            }
+        }
+    })
+
     $('#birth-date').mask('00/00/0000', {
-        placeholder: "__/__/____"
+        placeholder: "DD/MM/AAAA"
     });
 
     $('#cep').mask('00000-000', {
-        placeholder: "_____-___"
+        placeholder: "00000-000"
     });
 
     $('#phone').mask('(00) 0 0000-0000', {
-        placeholder: "(__) _ ____-____"
+        placeholder: "(DDD) 0 0000-0000"
     });
 
-    $('#number-address').mask('0000');
+    $('#number-address').mask('0000', {
+        placeholder: '000'
+    });
+
+    $('#state').mask('SS', {
+        placeholder: 'UF'
+    });
 })
 
+const birth = document.getElementById('birth-date');
+const cep = document.getElementById('cep');
 const form = document.querySelector('.form');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
+const errorBirth = document.getElementById('error-birth');
+const errorCep = document.getElementById('error-cep');
 const errorEmail = document.getElementById('error-email');
 const errorPhone = document.getElementById('error-phone');
+
+birth.addEventListener('blur', () => {
+    if (birth.value.length < 10) {
+        errorBirth.style.display = "flex";
+        birth.style.borderColor = "#DC2626";
+    } else {
+        errorBirth.style.display = "none";
+        birth.style.borderColor = "";
+    }
+})
+
+cep.addEventListener('blur', () => {
+    if (cep.value.length < 9) {
+        errorCep.style.display = "flex";
+        cep.style.borderColor = "#DC2626";
+    } else {
+        errorCep.style.display = "none";
+        cep.style.borderColor = "";
+    }
+})
 
 email.addEventListener('blur', () => {
     if (!(email.value.includes('@')) || !(email.value.includes('.com'))) {
@@ -32,7 +95,6 @@ email.addEventListener('blur', () => {
     }
 })
 
-
 phone.addEventListener('blur', () => {
     if (phone.value.length < 16) {
         errorPhone.style.display = "flex";
@@ -40,5 +102,32 @@ phone.addEventListener('blur', () => {
     } else {
         errorPhone.style.display = "none";
         phone.style.borderColor = "";
+    }
+})
+
+const inputFile = document.getElementById("birth-document");
+const pictureDocument = document.getElementById('picture-document');
+const pictureDocumentText = '<i class="hgi hgi-stroke hgi-cloud-upload"></i><p>Clique aqui para selecionar arquivos</p>';
+pictureDocument.innerHTML = pictureDocumentText;
+
+inputFile.addEventListener('change', (e) => {
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', (e) => {
+            const readerTarget = e.target;
+            const img = document.createElement('img');
+            img.src = readerTarget.result;
+            
+            pictureDocument.innerHTML = '';
+            pictureDocument.appendChild(img);
+        })
+
+        reader.readAsDataURL(file);
+    } else {
+        pictureDocument.innerHTML = pictureDocumentText;
     }
 })
